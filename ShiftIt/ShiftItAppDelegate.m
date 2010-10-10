7af8435d6381d0c27f8644e5c4c436824add1d97
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2010-10-10 20:06:20 +0400

    Refactored - (void)registerForLogin_ code.

--- ShiftIt/ShiftItAppDelegate.m
+++ ShiftIt/ShiftItAppDelegate.m
@@ -169,59 +169,51 @@ NSDictionary *allShiftActions = nil;
 
 - (void)registerForLogin_{
 	BOOL login = [[NSUserDefaults standardUserDefaults] boolForKey:@"shiftItstartLogin"];
-    if(login){
-        NSString * appPath = [[NSBundle mainBundle] bundlePath];
-        
-        // This will retrieve the path for the application
-        // For example, /Applications/test.app
-        CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
-        
-        // Create a reference to the shared file list.
-        // We are adding it to the current user only.
-        // If we want to add it all users, use
-        // kLSSharedFileListGlobalLoginItems instead of
-        // kLSSharedFileListSessionLoginItems
-        LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,kLSSharedFileListSessionLoginItems, NULL);
-        if (loginItems) {
-            //Insert an item to the list.
-            LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,kLSSharedFileListItemLast, NULL, NULL,url, NULL, NULL);
-            if (item){
-                CFRelease(item);
-            }
-        }	
-        CFRelease(loginItems);
-    }else {
-        NSString * appPath = [[NSBundle mainBundle] bundlePath];
-        
-        // This will retrieve the path for the application
-        // For example, /Applications/test.app
-        CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
-        
-        // Create a reference to the shared file list.
-        LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
-                                                                kLSSharedFileListSessionLoginItems, NULL);
-        
-        if (loginItems) {
-            UInt32 seedValue;
-            // Retrieve the list of Login Items and cast them to
-            // a NSArray so that it will be easier to iterate.
-            NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
-            int i = 0;
-            for(i ; i< [loginItemsArray count]; i++){
-                LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
-                                                                            objectAtIndex:i];
-                //Resolve the item with URL
-                if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
-                    NSString * urlPath = [(NSURL*)url path];
-                    if ([urlPath compare:appPath] == NSOrderedSame){
-                        LSSharedFileListItemRemove(loginItems,itemRef);
-                    }
-                }
-            }
-            [loginItemsArray release];
-        }
+
+	NSString *appPath = [[NSBundle mainBundle] bundlePath];
+	
+	// This will retrieve the path for the application
+	// For example, /Applications/test.app
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath]; 
+	
+	// Create a reference to the shared file list.
+	// We are adding it to the current user only.
+	// If we want to add it all users, use
+	// kLSSharedFileListGlobalLoginItems instead of
+	// kLSSharedFileListSessionLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL, kLSSharedFileListSessionLoginItems, NULL);
+	if (NULL == loginItems) {
+		return;
+	}
+	
+	if (login) {
+		//Insert an item to the list.
+		LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems, kLSSharedFileListItemLast, NULL, NULL, url, NULL, NULL);
+		if (item) {
+			CFRelease(item);
+		}
+	} else {
+		UInt32 seedValue;
+		int i;
+		
+		// Retrieve the list of Login Items and cast them to
+		// a NSArray so that it will be easier to iterate.
+		NSArray *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+
+		for(i = 0; i < [loginItemsArray count]; i++){
+			LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray objectAtIndex:i];
+			//Resolve the item with URL
+			if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*)&url, NULL) == noErr) {
+				NSString *urlPath = [(NSURL*)url path];
+				if ([urlPath compare:appPath] == NSOrderedSame){
+					LSSharedFileListItemRemove(loginItems, itemRef);
+				}
+			}
+		}
+		[loginItemsArray release];
     }
-    
+	
+	CFRelease(loginItems);
 }
 
 - (IBAction)showPreferences:(id)sender {
