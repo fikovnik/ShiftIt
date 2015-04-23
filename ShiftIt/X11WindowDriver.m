3c8b50ff71d9cf15c54d2a164d2bd222e6eb0cdc
Jean-Philippe Ouellet <jpo@vt.edu>
2015-04-22 20:40:54 -0400

    Build with MacPorts Xlib if present
    and prefer MacPorts over XQuartz and regular OS X versions which
    are rather old. (Even older than the latest xorg security errata.)

--- ShiftIt/X11WindowDriver.m
+++ ShiftIt/X11WindowDriver.m
@@ -35,10 +35,10 @@ NSInteger const kX11WindowDriverErrorCode = 20105;
 // TODO: extract
 static char *X11Paths_[] = {
 	"libX11.6.dylib",
-    "/usr/local/X11/libX11.6.dylib", // anyone?
-    "/opt/X11/lib/libX11.6.dylib", // XQuartz, needs be tried before regular
-	"/usr/X11/lib/libX11.6.dylib", // regular
+	"/usr/local/X11/libX11.6.dylib", // anyone?
 	"/opt/local/lib/libX11.6.dylib", // MacPorts
+	"/opt/X11/lib/libX11.6.dylib", // XQuartz, needs be tried before regular
+	"/usr/X11/lib/libX11.6.dylib", // regular
 	"/sw/X11/lib/libX11.6.dylib", // Fink?
 };
 
